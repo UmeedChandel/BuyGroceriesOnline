6a8c56f2410eb7e2dd0526a40701d6b94c6f77df
umeedchandel <umeedchandel@gmail.com>
2022-08-17 15:43:49 +0530

    Added Two Controller

---
+++ BuyGroceriesOnline/Controllers/ProductController.cs
@@ -0,0 +1,56 @@
+﻿using BuyGroceriesOnline.Models;
+using BuyGroceriesOnline.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuyGroceriesOnline.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        private IEnumerable<Product> GetAllProduct()
+        {
+            var products = _productRepository.AllProduct ;
+            return products;
+        }
+
+        public IActionResult List(int id)
+        {
+            IEnumerable<Product> product;
+            CustomClass customClass = new CustomClass();
+            if (id > 0)
+            {
+                product = GetAllProduct().Where(p => p.CategoryId == id);
+                customClass.CurrentCategory = product.FirstOrDefault().Category.CategoryName;
+                customClass.CategoryDescription = product.FirstOrDefault().Category.Description ;
+            }
+            else
+            {
+                product = GetAllProduct();
+                customClass.CurrentCategory = "List Of Products";
+                customClass.CategoryDescription = "";
+            }
+
+            customClass.Products = product;
+            return View(customClass);
+        }
+
+        public IActionResult ProductOfWeek()
+        {
+            return View(_productRepository.ProductOfTheWeek);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            return View(GetAllProduct().FirstOrDefault(p => p.ProductId == id));
+        }
+    }
+}
---
+++ BuyGroceriesOnline/ViewModels/CustomClass.cs
@@ -0,0 +1,11 @@
+﻿using BuyGroceriesOnline.Models;
+
+namespace BuyGroceriesOnline.ViewModels
+{
+    public class CustomClass
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public string CurrentCategory { get; set; }
+        public string CategoryDescription { get; set; }
+    }
+}

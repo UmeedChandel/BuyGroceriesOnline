6354b34e9d35a295af81482eb8f72cd6fc97aba9
aman.asati <aman.asati@BLR1-LHP-N73356>
2022-08-18 09:38:46 +0530

    Improved UI

--- BuyGroceriesOnline/Migrations/20220817171439_inititalMigration.cs
+++ BuyGroceriesOnline/Migrations/20220817171439_inititalMigration.cs
@@ -4,7 +4,7 @@
 
 namespace BuyGroceriesOnline.Migrations
 {
-    public partial class InitialMigration : Migration
+    public partial class inititalMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -20,6 +20,23 @@ namespace BuyGroceriesOnline.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    FeedbackId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    FeedBack = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.FeedbackId);
                 });
 
             migrationBuilder.CreateTable(
@@ -58,6 +75,9 @@ namespace BuyGroceriesOnline.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
             migrationBuilder.DropTable(
                 name: "Products");
 

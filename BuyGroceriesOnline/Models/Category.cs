4e2d62c7111968fcbeeb9894281974627595b5de
umeedchandel <umeedchandel@gmail.com>
2022-08-18 12:38:18 +0530

    View Component Category Menu

--- BuyGroceriesOnline/Models/Category.cs
+++ BuyGroceriesOnline/Models/Category.cs
@@ -1,7 +1,12 @@
-﻿namespace BuyGroceriesOnline.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BuyGroceriesOnline.Models
 {
     public class Category
     {
+        [BindNever]
+        [ValidateNever]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string Description { get; set; }

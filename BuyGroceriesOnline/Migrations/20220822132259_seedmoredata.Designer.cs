3abb1201c5f8b4f9016edc66985c92ec9b381adc
umeedchandel <umeedchandel@gmail.com>
2022-08-22 22:26:33 +0530

    UI changes and non in stock check

--- BuyGroceriesOnline/Migrations/20220822132259_seedmoredata.Designer.cs
+++ BuyGroceriesOnline/Migrations/20220822132259_seedmoredata.Designer.cs
@@ -4,6 +4,7 @@ using BuyGroceriesOnline.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BuyGroceriesOnline.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220822132259_seedmoredata")]
+    partial class seedmoredata
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

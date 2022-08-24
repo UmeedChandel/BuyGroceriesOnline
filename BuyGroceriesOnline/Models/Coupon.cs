bc322b8d1d5bee179f6ea1dcfe58549482c52d7c
aman.asati <aman.asati@BLR1-LHP-N73356>
2022-08-24 15:24:24 +0530

    UI Changes

--- BuyGroceriesOnline/Models/Coupon.cs
+++ BuyGroceriesOnline/Models/Coupon.cs
@@ -1,8 +1,11 @@
-﻿namespace BuyGroceriesOnline.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyGroceriesOnline.Models
 {
     public class Coupon
     {
         public int CouponId { get; set; }
+        [Display(Name = "Coupon Name")]
         public string CouponName { get; set; }
         public int Discount { get; set; }
         public List<Order> Orders { get; set; }
